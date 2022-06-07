67a4df5f54217a88a9151a3feda8c977ec3f9c61
Chris Gilmore <christopher.m.gilmore@gmail.com>
2022-06-07 14:38:41 -0400

    Post Event

--- force-app/main/default/aura/Dreampass_RegistrationFlowComponent/Dreampass_RegistrationFlowComponentController.js
+++ force-app/main/default/aura/Dreampass_RegistrationFlowComponent/Dreampass_RegistrationFlowComponentController.js
@@ -4,7 +4,9 @@
         // Find the component whose aura:id is "flowData"
         var flow = component.find("flowData");
         // In that component, start your flow. Reference the flow's API Name.
-        flow.startFlow("Davos_Registration_Flow");
+        //flow.startFlow("Davos_Registration_Flow");
+        //flow.startFlow("Davos_Self_Service_Checkin");
+        flow.startFlow("Davos_Self_Service_Checkin");
     },
     
     handleStatusChange : function (component, event, helper)
